b3e1b9fe8d577765d52ee5341f9d83b7a0ae50ff
Johan Berg <47738833+bergjohan@users.noreply.github.com>
2019-10-01 23:19:11 +0200

    Add docker-run.sh

---
+++ docker-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# $1 - The slug of the exercise (e.g. two-fer).
+# $2 - A relative path to an input directory containing the submitted solution file(s) and the necessary test file(s).
+
+docker run --rm -v "$(pwd)"/"$2":/mnt/exercism-iteration -v "$(pwd)"/output:/output test-runner "$1" /mnt/exercism-iteration/ /output/
